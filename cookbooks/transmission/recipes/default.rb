ffddce09096693449834a679ac46c641d31beab7
Bobby House <bobbyh.house@nuix.com>
2014-11-28 15:42:32 -0500

    wip;

---
+++ cookbooks/transmission/recipes/default.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook Name:: transmission
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+transmission_pkgs = value_for_platform(
+  ["debian", "ubuntu"] => {
+    "default" => ["transmission", "transmission-cli", "transmission-daemon"]
+  },
+  "default" => ["transmission", "transmission-cli", "transmission-daemon"]
+)
+
+transmission_pkgs.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
