b4588e6ad7cd75cb6d2dec660a54b7cbec5d19f0
Bobby House <bobbyh.house@nuix.com>
2014-11-29 19:30:18 -0500

    add mount

--- cookbooks/share-files/recipes/default.rb
+++ cookbooks/share-files/recipes/default.rb
@@ -9,15 +9,19 @@
 
 include_recipe "nfs::server"
 
-package "usbmount" do
-  action :install
-end
-
 directory "/var/nfs/share" do
   owner 'nobody'
   group 'nogroup'
   mode 0644
   action :create
+end
+
+mount "/var/nfs/share" do
+  device "703fcc7e-c4de-4fa9-8ae3-5fa6827ab658"
+  device_type :uuid
+  fstype "auto"
+  options "rw,user"
+  action [:mount, :enable]
 end
 
 nfs_export "/var/nfs/share" do
